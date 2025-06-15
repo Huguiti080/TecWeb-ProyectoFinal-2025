10317e0d86a51f9bdc86cac6fccd696aef895c85
Hugo VH <vazquezhugo608@gmail.com>
2025-06-14 20:39:08 -0600

    environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: "tecweb-proyectofinal-2025",
+    appId: "1:891151412097:web:f01e4bdb0c7c14b73cd680",
+    storageBucket: "tecweb-proyectofinal-2025.firebasestorage.app",
+    apiKey: "AIzaSyD1GdEEnLBoChteMMl7VMvTbbhy2NfpWRg",
+    authDomain: "tecweb-proyectofinal-2025.firebaseapp.com",
+    messagingSenderId: "891151412097",
+    measurementId: "G-E64BR5P5FZ"
+  }
+};
