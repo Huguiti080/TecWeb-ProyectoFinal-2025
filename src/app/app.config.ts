07c5b9c16ba7d618bcb2633c915cd8bea939d2a8
AndreVEAC <124841405+AndreVEAC@users.noreply.github.com>
2025-06-25 16:41:54 -0600

    Antes de actualizar Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -60,6 +60,9 @@ export const appConfig: ApplicationConfig = {
     provideAnalytics(() => getAnalytics()), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           }),
 
   
